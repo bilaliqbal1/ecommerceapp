650e518e4694aa76189d982f703b37fd1c8e6f25
bilaliqbal1 <muhammadbilaliqbal98@gmai.com>
2021-06-20 00:45:07 +0500

    Initial commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+
+const products  = [
+    {
+        id: 1,
+        name: 'Shoes',
+        description: 'Running shoes'
+    },
+    {
+        id: 2,
+        name: 'watch',
+        description: 'Running watch'
+    },
+    {
+        id: 3,
+        name: 'glasses',
+        description: 'Running glasses'
+    }
+];
+
+const Products = () =>{
+    <main>
+        <Grid container justify="center" spacing={4}>
+
+        </Grid>
+    </main>
+}
+
+export default Products;
