2b649a5bab7f20ae20c04d4fc9dd06c0fb61dc93
bilaliqbal1 <muhammadbilaliqbal98@gmai.com>
2021-06-28 18:17:20 +0500

    countries

--- src/components/Checkout/AddressForm.jsx
+++ src/components/Checkout/AddressForm.jsx
@@ -8,18 +8,25 @@ import {commerce} from '../../lib/commerce';
 const AddressForm = ({checkoutToken}) => {
     const methods = useForm();
 
-    const [shipping, setshipping] = useState([]);
-    const [countries, setCountries] = useState([]);
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState([]);
     const [divisions, setDivisions] = useState([]);
     const [division, setDivision] = useState('');
     const [shippingOptions, setShippingOptions] = useState([]);
     const [shippingOption, setShippingOption] = useState([]);
 
+    const countries = Object.entries(shippingCountries).map(([code, name]) => (
+        {id: code, lable:name}
+    ));
+    // console.log(countries);
+
     const fetchCountries = async (checkoutTokenId) =>{
         const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId);
 
             // console.log(countries);
-        setCountries(countries);
+            setShippingCountries(countries)
+            setShippingCountry(Object.keys(countries)[0]);
+
     }
     useEffect(()=>{
         fetchCountries(checkoutToken.id)
@@ -40,22 +47,27 @@ const AddressForm = ({checkoutToken}) => {
                         <CustomField required name="Zip" label="Zip /Post code" />
                             <Grid item xs={12} sm={6}>
                                 <InputLabel>Shipping Country</InputLabel>
-                                {/* <Select value={} fullWidth onchange={}>
+                                <Select value={shippingCountry} fullWidth onchange={(e)=> setShippingCountry(e.target.value)}>
+                                    {countries.map((country)=>(
+                                        <MenuItem key={country.id} value={country.id}>
+                                            {country.label}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                {/* <InputLabel>Shipping Subdivisions</InputLabel> */}
+                                {/* {counties.map((country)=>(
+
+                                ))}
+                                <Select value={} fullWidth onchange={}>
                                     <MenuItem key={}>
                                         Select Me
                                     </MenuItem>
                                 </Select> */}
                             </Grid>
                             <Grid item xs={12} sm={6}>
-                                <InputLabel>Shipping Subdivisions</InputLabel>
-                                {/* <Select value={} fullWidth onchange={}>
-                                    <MenuItem key={}>
-                                        Select Me
-                                    </MenuItem>
-                                </Select> */}
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <InputLabel>Shipping Country</InputLabel>
+                                {/* <InputLabel>Shipping Country</InputLabel> */}
                                 {/* <Select value={} fullWidth onchange={}>
                                     <MenuItem key={}>
                                         Select Me
